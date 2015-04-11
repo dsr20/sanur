6d0a30624a130b2b0f5e16dbbc07f1fd5fcc1d6b
sanurDSS <ivanps94@gmail.com>
2015-04-11 15:08:05 +0200

    Crear paciente finalizado (2). Fallo al añadir los archivos resuelto.

---
+++ sanur/SanurGen/SanurGenNHibernate/Form_crear_paciente.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace SanurGenNHibernate
+{
+    partial class Form_crear_paciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TTelefono = new System.Windows.Forms.TextBox();
+            this.TDireccion = new System.Windows.Forms.TextBox();
+            this.TNacionalidad = new System.Windows.Forms.TextBox();
+            this.TSip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.TCP = new System.Windows.Forms.TextBox();
+            this.TGS = new System.Windows.Forms.TextBox();
+            this.TMunicipio = new System.Windows.Forms.TextBox();
+            this.TEmail = new System.Windows.Forms.TextBox();
+            this.TDNI = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.TIPS = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label15 = new System.Windows.Forms.Label();
+            this.selector_sexo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(269, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Atrás";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dar de alta un nuevo paciente";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TNombre
+            // 
+            this.TNombre.BackColor = System.Drawing.Color.White;
+            this.TNombre.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.TNombre.Location = new System.Drawing.Point(184, 43);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(283, 20);
+            this.TNombre.TabIndex = 3;
+            this.TNombre.TextChanged += new System.EventHandler(this.TNombre_TextChanged);
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(184, 69);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(283, 20);
+            this.TApellido.TabIndex = 4;
+            // 
+            // TTelefono
+            // 
+            this.TTelefono.Location = new System.Drawing.Point(184, 136);
+            this.TTelefono.Name = "TTelefono";
+            this.TTelefono.Size = new System.Drawing.Size(283, 20);
+            this.TTelefono.TabIndex = 5;
+            // 
+            // TDireccion
+            // 
+            this.TDireccion.Location = new System.Drawing.Point(184, 162);
+            this.TDireccion.Name = "TDireccion";
+            this.TDireccion.Size = new System.Drawing.Size(283, 20);
+            this.TDireccion.TabIndex = 6;
+            // 
+            // TNacionalidad
+            // 
+            this.TNacionalidad.Location = new System.Drawing.Point(184, 188);
+            this.TNacionalidad.Name = "TNacionalidad";
+            this.TNacionalidad.Size = new System.Drawing.Size(283, 20);
+            this.TNacionalidad.TabIndex = 7;
+            // 
+            // TSip
+            // 
+            this.TSip.Location = new System.Drawing.Point(184, 214);
+            this.TSip.Name = "TSip";
+            this.TSip.Size = new System.Drawing.Size(283, 20);
+            this.TSip.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Apellidos";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Telefono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Dirección";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nacionalidad";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "SIP";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 370);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "CP";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 344);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "grupo sanduineo";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(18, 318);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(51, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "municipio";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(18, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "email";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(20, 266);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "sexo";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(20, 243);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(26, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "DNI";
+            // 
+            // TCP
+            // 
+            this.TCP.Location = new System.Drawing.Point(184, 370);
+            this.TCP.Name = "TCP";
+            this.TCP.Size = new System.Drawing.Size(283, 20);
+            this.TCP.TabIndex = 20;
+            // 
+            // TGS
+            // 
+            this.TGS.Location = new System.Drawing.Point(184, 344);
+            this.TGS.Name = "TGS";
+            this.TGS.Size = new System.Drawing.Size(283, 20);
+            this.TGS.TabIndex = 19;
+            // 
+            // TMunicipio
+            // 
+            this.TMunicipio.Location = new System.Drawing.Point(184, 318);
+            this.TMunicipio.Name = "TMunicipio";
+            this.TMunicipio.Size = new System.Drawing.Size(283, 20);
+            this.TMunicipio.TabIndex = 18;
+            this.TMunicipio.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // TEmail
+            // 
+            this.TEmail.Location = new System.Drawing.Point(184, 292);
+            this.TEmail.Name = "TEmail";
+            this.TEmail.Size = new System.Drawing.Size(283, 20);
+            this.TEmail.TabIndex = 17;
+            // 
+            // TDNI
+            // 
+            this.TDNI.Location = new System.Drawing.Point(184, 240);
+            this.TDNI.Name = "TDNI";
+            this.TDNI.Size = new System.Drawing.Size(283, 20);
+            this.TDNI.TabIndex = 15;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(16, 396);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(32, 13);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "p_ips";
+            // 
+            // TIPS
+            // 
+            this.TIPS.Location = new System.Drawing.Point(184, 396);
+            this.TIPS.Name = "TIPS";
+            this.TIPS.Size = new System.Drawing.Size(283, 20);
+            this.TIPS.TabIndex = 27;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(184, 95);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 29;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(19, 95);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(93, 13);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Fecha Nacimiento";
+            // 
+            // selector_sexo
+            // 
+            this.selector_sexo.FormattingEnabled = true;
+            this.selector_sexo.Items.AddRange(new object[] {
+            "Hombre",
+            "Mujer"});
+            this.selector_sexo.Location = new System.Drawing.Point(184, 265);
+            this.selector_sexo.Name = "selector_sexo";
+            this.selector_sexo.Size = new System.Drawing.Size(200, 21);
+            this.selector_sexo.TabIndex = 31;
+            // 
+            // Form_crear_paciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 468);
+            this.Controls.Add(this.selector_sexo);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.TIPS);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.TCP);
+            this.Controls.Add(this.TGS);
+            this.Controls.Add(this.TMunicipio);
+            this.Controls.Add(this.TEmail);
+            this.Controls.Add(this.TDNI);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TSip);
+            this.Controls.Add(this.TNacionalidad);
+            this.Controls.Add(this.TDireccion);
+            this.Controls.Add(this.TTelefono);
+            this.Controls.Add(this.TApellido);
+            this.Controls.Add(this.TNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form_crear_paciente";
+            this.Text = "SANUR - Creación Paciente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TTelefono;
+        private System.Windows.Forms.TextBox TDireccion;
+        private System.Windows.Forms.TextBox TNacionalidad;
+        private System.Windows.Forms.TextBox TSip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox TCP;
+        private System.Windows.Forms.TextBox TGS;
+        private System.Windows.Forms.TextBox TMunicipio;
+        private System.Windows.Forms.TextBox TEmail;
+        private System.Windows.Forms.TextBox TDNI;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox TIPS;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.ComboBox selector_sexo;
+    }
+}
---
+++ sanur/SanurGen/SanurGenNHibernate/Form_crear_paciente.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SanurGenNHibernate.EN.Sanur;
+using SanurGenNHibernate.CEN.Sanur;
+
+namespace SanurGenNHibernate
+{
+    public partial class Form_crear_paciente : Form
+    {
+        public Form_crear_paciente()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PacienteEN pacienteEn=new PacienteEN();
+            
+            // Validación de los datos (versión rápida enfocada a compatibilidad con la BD)
+            if(TNombre.Text.Length < 1)
+                MessageBox.Show("El nombre del paciente no puede ser vacío");
+            else if (TApellido.Text.Length < 1)
+                MessageBox.Show("El apellido del paciente no puede ser vacío");
+            else if (TTelefono.Text.Length != 9)
+                MessageBox.Show("El telefono del paciente no puede ser vacío o es incorrecto");
+            else if (TDireccion.Text.Length < 1)
+                MessageBox.Show("La direccion del paciente no puede ser vacío");
+            else if (TNacionalidad.Text.Length < 1)
+                MessageBox.Show("La nacionalidad del paciente no puede ser vacío");
+            else if (TDNI.Text.Length != 8)
+                MessageBox.Show("El DNI tiene un formato incorrecto");
+            else if (TEmail.Text.Length < 1)
+                MessageBox.Show("El email no puede estar vacío");
+            else if (TMunicipio.Text.Length < 1)
+                MessageBox.Show("El municipio no puede estar vacío");
+            else if (TGS.Text.Length < 1)
+                MessageBox.Show("El grupo sanguíneo no puede estar vacío");
+            else if (TCP.Text.Length < 1)
+                MessageBox.Show("El código postal no puede estar vacío");
+            else if (TIPS.Text.Length < 1)
+                MessageBox.Show("El campo IPS no puede ser vacío");
+            else
+            {
+
+                PacienteCEN pacienteCen = new PacienteCEN();
+                pacienteCen.New_(TNombre.Text, TApellido.Text, dateTimePicker1.Value, TTelefono.Text, TDireccion.Text, TNacionalidad.Text, Convert.ToInt32(TSip.Text), Convert.ToInt32(TDNI.Text), selector_sexo.Text, TEmail.Text, TMunicipio.Text, TGS.Text, TCP.Text, TIPS.Text);
+            }
+
+
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
