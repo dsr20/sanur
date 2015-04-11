ac702821e900d838495ad46a8edc79048c931f1c
sanurDSS <ivanps94@gmail.com>
2015-04-11 12:37:37 +0200

    Creación Paciente finalizado

--- sanur/SanurGen/SanurGenNHibernate/Program.cs
+++ sanur/SanurGen/SanurGenNHibernate/Program.cs
@@ -15,7 +15,7 @@ namespace SanurGenNHibernate
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
+            //Application.Run(new Form_busca_paciente());
             Login login = new Login();
             login.ShowDialog();
         }
