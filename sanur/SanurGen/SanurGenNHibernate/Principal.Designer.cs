24a98f74d3d37c1736955591af5ea0bf64b841bb
Gacel <gacel.1991@gmail.com>
2015-05-12 16:57:58 +0200

    Revert "Revert "barra de tareas""
    This reverts commit 57f8f8fceb4d52c2b3cbc91845f08a0785ed47c1.

--- sanur/SanurGen/SanurGenNHibernate/Principal.Designer.cs
+++ sanur/SanurGen/SanurGenNHibernate/Principal.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
             this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
@@ -64,6 +65,8 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.menuStrip1.BackgroundImage = global::SanurGenNHibernate.Properties.Resources.barra2;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.usuariosToolStripMenuItem,
             this.pacientesToolStripMenuItem,
@@ -77,6 +80,7 @@
             // 
             // usuariosToolStripMenuItem
             // 
+            this.usuariosToolStripMenuItem.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.usuariosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.crearUsuariosToolStripMenuItem,
             this.modificarUsuariosToolStripMenuItem,
@@ -89,6 +93,7 @@
             // 
             // crearUsuariosToolStripMenuItem
             // 
+            this.crearUsuariosToolStripMenuItem.BackgroundImage = global::SanurGenNHibernate.Properties.Resources.barrainf;
             this.crearUsuariosToolStripMenuItem.Name = "crearUsuariosToolStripMenuItem";
             this.crearUsuariosToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.crearUsuariosToolStripMenuItem.Text = "Crear Usuarios";
@@ -96,6 +101,7 @@
             // 
             // modificarUsuariosToolStripMenuItem
             // 
+            this.modificarUsuariosToolStripMenuItem.BackgroundImage = global::SanurGenNHibernate.Properties.Resources.barrainf;
             this.modificarUsuariosToolStripMenuItem.Name = "modificarUsuariosToolStripMenuItem";
             this.modificarUsuariosToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.modificarUsuariosToolStripMenuItem.Text = "Modificar Usuario";
@@ -103,6 +109,7 @@
             // 
             // borrarUsuarioToolStripMenuItem
             // 
+            this.borrarUsuarioToolStripMenuItem.BackgroundImage = global::SanurGenNHibernate.Properties.Resources.barrainf;
             this.borrarUsuarioToolStripMenuItem.Name = "borrarUsuarioToolStripMenuItem";
             this.borrarUsuarioToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
             this.borrarUsuarioToolStripMenuItem.Text = "Borrar Usuario";
@@ -120,6 +127,7 @@
             // 
             // nuevoPacienteToolStripMenuItem
             // 
+            this.nuevoPacienteToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nuevoPacienteToolStripMenuItem.BackgroundImage")));
             this.nuevoPacienteToolStripMenuItem.Name = "nuevoPacienteToolStripMenuItem";
             this.nuevoPacienteToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
             this.nuevoPacienteToolStripMenuItem.Text = "Nuevo Paciente";
@@ -127,6 +135,7 @@
             // 
             // buscarPacienteToolStripMenuItem
             // 
+            this.buscarPacienteToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buscarPacienteToolStripMenuItem.BackgroundImage")));
             this.buscarPacienteToolStripMenuItem.Name = "buscarPacienteToolStripMenuItem";
             this.buscarPacienteToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
             this.buscarPacienteToolStripMenuItem.Text = "Buscar Paciente";
@@ -143,6 +152,7 @@
             // 
             // nuevoTriageToolStripMenuItem
             // 
+            this.nuevoTriageToolStripMenuItem.BackgroundImage = global::SanurGenNHibernate.Properties.Resources.barrainf;
             this.nuevoTriageToolStripMenuItem.Name = "nuevoTriageToolStripMenuItem";
             this.nuevoTriageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.nuevoTriageToolStripMenuItem.Text = "Nuevo Triage";
